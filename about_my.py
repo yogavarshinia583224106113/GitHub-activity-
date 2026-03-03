77a396feb87d2909675d2b4669c5d5c4886e1a19
yogavarshinia583224106113 <yogavarshinia583224106113@nprcolleges.org>
2026-03-03 18:45:06 +0530

    Create about_my.py

---
+++ about_my.py
@@ -0,0 +1,13 @@
+
+
+name = "Yogavarshini"
+course = "Python Programming"
+college = "Engineering Student"
+
+print("Hello! 👋")
+print("My name is", name)
+print("I am currently learning", course)
+print("I am an", college)
+print("This is my GitHub activity repository.")
+print("I am excited to improve my coding skills! 🚀")
+print("Thank you for visiting my project! 😊")
